9b539e043f14d60603c5785d6f8441f40cfc1aa1
iamfuzzi <geometryposter@gmail.com>
2025-03-01 20:47:57 +0300

    Добавление счетчика статических файлов

---
+++ src/main/java/me/fuzzi/breeze/core/Variables.java
@@ -0,0 +1,19 @@
+package me.fuzzi.breeze.core;
+
+public class Variables {
+    private static int page = 0;
+    public static void page() {
+        page++;
+    }
+    public static int getPage() {
+        return page;
+    }
+
+    private static int statics = 0;
+    public static int getStatics() {
+        return statics;
+    }
+    public static void statics() {
+        statics++;
+    }
+}
