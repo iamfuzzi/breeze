4e2516ba6373d72023cccb755387399603ef527f
iamfuzzi <geometryposter@gmail.com>
2025-03-03 21:02:34 +0300

    Добавление комментариев

--- src/main/java/me/fuzzi/breeze/core/WebPage.java
+++ src/main/java/me/fuzzi/breeze/core/WebPage.java
@@ -7,7 +7,16 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * <p>Класс создания динамических страниц внутри Java-кода.</p>
+ * <p>Поддерживают переменную exchange (HttpExchange).</p>
+ * <p>Важно, чтобы все переменные или значения, использующие exchange, были установлены внутри метода page.</p>
+ * @author iamfuzzi
+ * @version 1.0
+ * @since 1.0
+ */
 public abstract class WebPage implements WebPageInterface {
+
     public WebPage(HttpServer server, String web) {
         this.server = server;
         this.web = web;
@@ -15,6 +24,11 @@ public abstract class WebPage implements WebPageInterface {
     private final String web;
     private final HttpServer server;
     protected HttpExchange exchange;
+
+    /**
+     * <p>Метод регистрации страницы на сервере.</p>
+     * @since 1.0
+     */
     public void load() {
         server.createContext(web, new HttpHandler() {
             @Override
