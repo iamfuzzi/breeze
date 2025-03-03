1e6a35a0a06c82049c0ffa26d862d606b604c4c5
iamfuzzi <geometryposter@gmail.com>
2025-03-03 21:02:24 +0300

    Добавление счетчиков контроллеров и плейсхолдеров. Добавление комментариев

--- src/main/java/me/fuzzi/breeze/core/Variables.java
+++ src/main/java/me/fuzzi/breeze/core/Variables.java
@@ -1,6 +1,14 @@
 package me.fuzzi.breeze.core;
 
+/**
+ * <p>Класс счетчиков. Содержит в себе эддеры (как сеттеры, но не принимают значение, просто добавляют к переменной +1) и геттеры переменных.</p>
+ * <p>Переменные этого класса используются для получения и установки информации о загруженных ресурсах.</p>
+ * @author iamfuzzi
+ * @version 1.0
+ * @since 1.0
+ */
 class Variables {
+
     private static int page = 0;
     public static void page() {
         page++;
@@ -15,5 +23,21 @@ class Variables {
     }
     public static void statics() {
         statics++;
+    }
+
+    private static int controller = 0;
+    public static int getController() {
+        return controller;
+    }
+    public static void controller() {
+        controller++;
+    }
+
+    private static int placeholder = 0;
+    public static int getPlaceholder() {
+        return placeholder;
+    }
+    public static void placeholder() {
+        placeholder++;
     }
 }
